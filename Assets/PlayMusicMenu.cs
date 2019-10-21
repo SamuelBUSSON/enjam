9e5e07d41ca0fb30e2c69d246b84cc6d9ed7ae87
Samuel Busson <samuel.busson@hotmail.fr>
2019-10-21 11:54:51 +0200

    Fix build bug

---
+++ Assets/PlayMusicMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusicMenu : MonoBehaviour
+{
+
+    uint id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        id = AkSoundEngine.PostEvent("Play_music_menu", gameObject); 
+        
+    }
+
+    private void OnDestroy()
+    {
+        AkSoundEngine.StopPlayingID(id);
+    }
+}
