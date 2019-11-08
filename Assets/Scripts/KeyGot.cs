9d0b4101424d9d80b74e9ded31c3592bbe11bf0a
Rémi Moreaux <moreaux_rmi@yahoo.fr>
2019-11-08 18:07:47 +0100

    BIG POLISH (Wall bis bars fix, change directional light orientation, fix collisions of items (projectors, beds), new victory screen, bigger guillaume levieux, useless key)

---
+++ Assets/Scripts/KeyGot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyGot : MonoBehaviour
+{
+    private bool obtain;
+    private GameObject message;
+    // Start is called before the first frame update
+    void Start()
+    {
+        message = GameObject.Find("MessageCanvas");
+    }
+
+    void Update()
+    {
+        if (obtain == true)
+        {
+            transform.position += Vector3.up * Time.deltaTime*10;
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            obtain = true;
+            GetComponent<Animator>().SetBool("obtained", true);
+            AkSoundEngine.PostEvent("Load_game_play_sound", gameObject);
+            message.GetComponent<Animator>().SetBool("MsgAnimBool", true);
+        }
+    }
+}
